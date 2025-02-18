0797a61b0a225f4c6aedca78aa43715bc8f73bb4
Lscanferlato <43204622+LScanferlato@users.noreply.github.com>
2025-02-18 14:12:41 +0100

    Update app.py

--- app.py
+++ app.py
@@ -6,12 +6,12 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     # Data di riferimento
-    target_date = datetime(2030, 11, 30)
+    target_date = datetime(2030, 12, 31)
     # Data attuale
     current_date = datetime.now()
     # Calcolo dei giorni rimanenti
     days_remaining = (target_date - current_date).days
-    return f"<h1>Giorni rimanenti al 30/11/2030: {days_remaining}</h1>"
+    return f"<h1>Giorni rimanenti al 31/12/2030: {days_remaining}</h1>"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
