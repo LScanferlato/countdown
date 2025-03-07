f80415c709b48e62a5b2b9327bd955cf087f0e1b
Lscanferlato <43204622+LScanferlato@users.noreply.github.com>
2025-03-07 11:16:30 +0100

    2 righe

--- app.py
+++ app.py
@@ -6,12 +6,17 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     # Data di riferimento
-    target_date = datetime(2030, 12, 12)
+    target_date = datetime(2030, 12, 1)
+    max_target_date = datetime(2030, 12, 1)
     # Data attuale
     current_date = datetime.now()
     # Calcolo dei giorni rimanenti
     days_remaining = (target_date - current_date).days
-    return f"<h1>Giorni rimanenti al {target_date}: {days_remaining}</h1>"
+    maxdays_remaining = (max_target_date - current_date).days
+    # Formatta il testo di ritorno su due righe
+    return (
+        f"<h1>Giorni rimanenti al {target_date}: {days_remaining}</h1>\n"
+        f"<h2>Giorni massimi rimanenti al {max_target_date}: {maxdays_remaining}</h2>"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
